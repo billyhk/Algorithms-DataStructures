496a1a893cf7650d68f3baf285a91efcd61f1b35
Uldarico Estrada <uldarico@outlook.com>
2018-10-27 18:42:07 -0700

    Added two more solutions
    integerToStringOfFixedWidth, inCorrectPasswordAttempts

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,15 +1,19 @@
 function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
    let failedAttempts = 0;
 
+   // iterate through number of attempts 
    for (let attempt of attempts) {
 
-      failedAttempts = attempt === passcode ? 0 : failedAttempts;
+      // reset number of attempts once the user enters the password correctly
+      // otherwise, increment number of failed attempts by one 
+      failedAttempts = attempt === passcode ? 0 : failedAttempts++;
     
+      // check if the password was incorrect ten times 
       if(failedAttempts === 10) {
           return true; // lock user out of account
       }
    }
-
+   // do not log user out of his account
    return false;
 }
 
