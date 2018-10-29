797d8a6c796b00d0ad621ac0327f68c37c188d87
Uldarico Estrada <uldarico@outlook.com>
2018-10-29 16:55:40 -0700

    5 more solutions

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,5 +1,16 @@
 function isTandemRepeat(inputString: string): boolean {
-  
+    const wordLength = inputString.length;
+
+    if (wordLength % 2 === 0) {
+        const firstHalf = inputString.slice(0, wordLength / 2);
+        const secondHalf = inputString.slice(wordLength / 2, wordLength);
+    
+        // check if they can go in tandem
+        return firstHalf === secondHalf;
+    }
+    
+    // odd values cannot be cut in half equally
+    return false;
 }
 
 console.log(isTandemRepeat('tandemtandem'))
