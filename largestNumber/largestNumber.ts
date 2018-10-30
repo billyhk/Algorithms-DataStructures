8ac41aa351aa0deea039713f4ce7bd1eeae070e8
Uldarico Estrada <uldarico@outlook.com>
2018-10-29 19:22:17 -0700

    More solutions
    -largestNumber
    -lateRide
        + fixed typo in algorithm readme

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,9 +1,12 @@
 function largestNumber(n: number): number {
-    let word = ''; 
+    // let word = ''; 
 
-    for (let i = 0; i < n; i++) {
+    const word = '9'.repeat(n);
+
+    return parseInt(word);
+    /* for (let i = 0; i < n; i++) {
         word = word.concat('9');
-    }
+    } */
 
     return parseInt(word);
 }
